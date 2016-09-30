75440cdd1138dfda614d813f6d598d5e4f23d267
wangliliang <wangliliang@zizizizizi.com>
2016-09-30 17:11:10 +0800

    update

--- xyz/test/logic-test/function/test.sh
+++ xyz/test/logic-test/function/test.sh
@@ -1,3 +1,5 @@
+cd $(dirname $0)
+
 ../../bin/wll v5.xyz v5.in | diff - v5.out
-../../bin/wll v5.xyz v5.err.in 
-../../bin/wll v6.xyz v6.in
+#../../bin/wll v5.xyz v5.err.in 
+#../../bin/wll v6.xyz v6.in
--- xyz/test/logic-test/loop/test.sh
+++ xyz/test/logic-test/loop/test.sh
@@ -1,2 +1,4 @@
+cd $(dirname $0)
+
 ../../bin/wll l2.xyz l2.in | diff - l2.out 
 ../../bin/wll loop.xyz loop.in | diff - loop.out
