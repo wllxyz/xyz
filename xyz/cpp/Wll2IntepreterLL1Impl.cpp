81269f0b14d8ead67a10a2c28f59a8a531b468e8
wangliliang <wangliliang@zizizizizi.com>
2016-10-04 00:16:14 +0800

    恢复  依次求值语义

--- xyz/cpp/Wll2IntepreterLL1Impl.cpp
+++ xyz/cpp/Wll2IntepreterLL1Impl.cpp
@@ -75,11 +75,13 @@ bool Wll2IntepreterLL1Impl::IntepretSExpression(std::vector<Symbols>& result)
 		{
 			this->eval_switch = false;
 		}
+		/*
 		else if(symbol == Symbols::EVAL)
 		{
-			this->eval_switch = true;
-			local_eval_switch = true;
+			this->eval_switch = true;			//$EVAL的子节点开启求值(允许内层屏蔽节点先求值)
+			local_eval_switch = true;			//开启本层$EVAL命令的求值
 		}
+		*/
 
 		vector<vector<Symbols> >parameter_fields;
 		parameter_fields.push_back(cmd);
