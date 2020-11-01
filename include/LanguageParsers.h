a7b0ff5cd6850d6774eb71e7cae9e575beccf0af
fangfeng <fangfeng@wacai.com>
2020-11-01 19:53:41 +0800

    提交以往修改的代码

--- include/LanguageParsers.h
+++ include/LanguageParsers.h
@@ -13,9 +13,10 @@ class LanguageParsers
 {
 public:
 	LanguageParsers(Compiler* compiler) { this->compiler = compiler; }
-public:
 	//input symbols --> source parse grammar tree
 	virtual bool Parse(const LanguageGrammar& languages, const std::vector<Symbols>& input_symbols, LanguageTree*& source_tree, Symbols start_symbol) = 0;
+
+private:
 	Compiler* compiler;
 };//end of LanguageParsers
 
