07fb6e8c13d8713404cf108f06493843d3e0fde2
fangfeng <fangfeng@wacai.com>
2020-11-11 18:56:50 +0800

    更新文档

---
+++ test/grammar-improve/1.statement/test.sh
@@ -0,0 +1,5 @@
+wll grammar-statement.xyz s1.xyz s1.in
+wll grammar-statement.xyz empty-statement.xyz s1.in
+wll grammar-statement.xyz get_set.xyz s1.in
+wll grammar-statement.xyz if_statement.xyz s1.in
+
