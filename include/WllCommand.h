e36564b5fb8911bcda3da9f98ff44f627acebc5b
fangfeng <fangfeng@wacai.com>
2020-12-18 11:19:17 +0800

    添加CompactCommand

--- include/WllCommand.h
+++ include/WllCommand.h
@@ -99,6 +99,13 @@ public:
 	virtual bool Intepret(std::vector<Symbols>& result);
 };
 
+class CompactCommand : public WllCommand
+{
+public:
+	CompactCommand(Symbols cmd, std::vector< std::vector<Symbols> >& parameter_fields, WllIntepreter* intepreter);
+	virtual bool Intepret(std::vector<Symbols>& result);
+};
+
 class ListCommand : public WllCommand
 {
 public:
