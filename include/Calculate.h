24a2af060a4804946a7d77248fe9b9f4a649fdaf
fangfeng <fangfeng@wacai.com>
2020-12-18 11:12:35 +0800

    添加Encode和Flat函数

--- include/Calculate.h
+++ include/Calculate.h
@@ -68,8 +68,13 @@ bool Equal(const Symbols& a, const Symbols& b);
 bool LessThan(const Symbols& a, const Symbols& b);
 
 //把长度大于1的Symbols自动打包成COMPACT_SYMBOL类型
-Symbols Encode(vector<Symbols>& value);
+Symbols Encode(vector<Symbols>& value, bool force = false);
 //把COMPACT_SYMBOL类型的Symbols复原成vector<Symbols>数组
 vector<Symbols> Decode(Symbols& symbol);
+
+//递归展平COMPACT_SYMBOL
+void Flat(const Symbols& compacted_symbol, /* ==> */ vector<Symbols>& flated_symbols);
+//递归展平COMPACT_SYMBOL
+void Flat(const vector<Symbols>& symbols, /* ==> */ vector<Symbols>& flated_symbols);
 
 #endif //CALCULATE_H
