20b42b7498ba6c79d186a168ec60348a06296461
fangfeng <fangfeng@wacai.com>
2021-01-13 00:04:56 +0800

    add compare.sh

---
+++ test/logic-test/CalculateCommands/compare.sh
@@ -0,0 +1,3 @@
+time /home/wll/work/install/xyz/xyz-2.0.0/bin/wll calculate_commands-old.xyz null.in >old.result
+time /home/wll/work/install/xyz/xyz-2.0.2/bin/wll calculate_commands.xyz null.in >new.result
+
